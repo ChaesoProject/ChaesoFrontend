fa4864b5b0d82e0540c13967b4818efaf9d6e7d9
rochamanuela <rochadesouzamanuela@gmail.com>
2024-02-22 16:47:27 -0300

    22/02/2024 - 16:40

--- pages/app_client/Home/index.jsx
+++ pages/app_client/Home/index.jsx
@@ -4,10 +4,14 @@ import {
     View,
     ScrollView,
     TouchableOpacity,
+    Image,
+    FlatList,
 } from "react-native";
 import { CheckBox } from "react-native-elements";
 import Feather from "react-native-vector-icons/Feather";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import Entypo from "react-native-vector-icons/Entypo";
+import FontAwesome6 from "react-native-vector-icons/FontAwesome6";
 
 import ButtonGreen from "../../../components/ButtonGreen";
 import InputGray from "../../../components/InputGray";
@@ -23,6 +27,30 @@ export default function Home({ navigation }) {
     };
 
     const [search, setSearch] = useState("");
+
+    // renderização de cards
+    const DATA = [
+        { productName: 'Mamão', unit: '1 Kg', productPrice: '5,00', imageUrl: require('../../../assets/images/fruits/mamão.png') },
+        { productName: 'Maçã', unit: '1 Kg', productPrice: '3,00', imageUrl: require('../../../assets/images/fruits/maça.png') },
+        { productName: 'Uva Vitória', unit: '1 Kg', productPrice: '15,00', imageUrl: require('../../../assets/images/fruits/uva.png') },
+        { productName: 'Kiwi', unit: '1 Kg', productPrice: '20,00', imageUrl: require('../../../assets/images/fruits/kiwi.png') },
+    ];
+
+    const renderItem = ({ item }) => (
+        <View style={styles.item}>
+            <Image source={item.imageUrl} style={styles.imageCard} />
+            <View style={styles.rowProduct}>
+                <Text style={styles.productName}>{item.productName}</Text>
+                <Text style={styles.unit}>{item.unit}</Text>
+            </View>
+            <Text style={styles.productPrice}>R$ {item.productPrice}</Text>
+
+            <TouchableOpacity style={styles.buttonAdd}>
+                <FontAwesome6 name="cart-shopping" size={18} color="#008764" />
+                <Text style={styles.textAdd}>Adicionar</Text>
+            </TouchableOpacity>
+        </View>
+    );
 
     return (
         <View style={styles.container}>
@@ -57,6 +85,40 @@ export default function Home({ navigation }) {
                                 <Feather name="search" size={23} color="#FFF" />
                             </View>
                         </TouchableOpacity>
+                    </View>
+                    <Text style={styles.categoryTitle}>Categorias</Text>
+                    <View style={styles.rowCategories}>
+                        <TouchableOpacity style={styles.category}>
+                            <FontAwesome6 name="apple-whole" size={32} color="#EB3223" />
+                            <Text style={styles.categoryName}>Frutas</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.category}>
+                            <FontAwesome6 name="carrot" size={32} color="#F29D38" />
+                            <Text style={styles.categoryName}>Legumes</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.category}>
+                            <FontAwesome6 name="leaf" size={32} color="#78A55A" />
+                            <Text style={styles.categoryName}>Verduras</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <Image
+                        source={require("../../../assets/images/banner.png")}
+                        style={styles.imageBanner}
+                    />
+                    <View style={styles.bannerContainer}>
+                        <Text>textos aqui</Text>
+                        <Image
+                            source={require("../../../assets/images/vegetable-basket.png")}
+                            style={styles.imageB}
+                        />
+                    </View>
+                    <View style={styles.cardsContainer}>
+                        <FlatList
+                            data={DATA}
+                            renderItem={renderItem}
+                            keyExtractor={item => item.id}
+                            numColumns={2}
+                        />
                     </View>
                 </ScrollView>
             </View>
--- pages/app_client/Home/styles.jsx
+++ pages/app_client/Home/styles.jsx
@@ -3,12 +3,13 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#FFF',
+        backgroundColor: '#FEFEFE',
     },
     containerChild: {
         flex: 1,
-        backgroundColor: '#FFF',
+        backgroundColor: '#FEFEFE',
         padding: 30,
+        paddingTop: 35,
         justifyContent: 'top'
     },
     header: {
@@ -16,7 +17,8 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         width: '100%',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
+        marginBottom: 10,
     },
     headerTextContainer: {
         display: 'flex',
@@ -53,15 +55,129 @@ const styles = StyleSheet.create({
         backgroundColor: '#00916C'
     },
     mainContainer: {
-        marginTop: 30
+        paddingTop: 25
     },
     searchContainer: {
         display: 'flex',
         flexDirection: 'row',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
     },
     input: {
         width: '83.5%'
+    },
+    rowCategories: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: '100%',
+        marginBottom: 30
+    },
+    category: {
+        backgroundColor: '#FFFFFF',
+        borderWidth: 1,
+        borderColor: '#F2F2F2',
+        borderRadius: 8,
+        width: '32%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 11
+    },
+    categoryName: {
+        fontFamily: 'poppins_medium',
+        fontSize: 10,
+        color: '#000',
+        marginTop: 5
+    },
+    categoryTitle: {
+        color: '#000',
+        fontSize: 16,
+        fontFamily: 'poppins_medium',
+        marginTop: 21,
+        marginBottom: 14
+    },
+    imageBanner: {
+        width: '100%',
+        height: 140,
+        marginBottom: 30
+    },
+    cardsContainer: {
+        display: 'flex',
+    },
+
+    bannerContainer: {
+        width: '100%',
+        height: 130,
+        borderRadius: 10,
+        backgroundColor: '#F5F5F5',
+        marginBottom: 30,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: 20
+    },
+    imageB: {
+        width: 130,
+        height: 115
+    },
+
+    // renderização de cards
+    item: {
+        marginRight: 10,
+        marginBottom: 15,
+        backgroundColor: '#FFFFFF',
+        borderWidth: 1,
+        borderColor: '#ECECEC',
+        borderRadius: 8,
+        padding: 9,
+        flexBasis: `${(100 / 2)-2}%`,
+        alignItems: 'center'
+    },
+    imageCard: {
+        width: '100%',
+        height: 110,
+        marginBottom: 7,
+        borderRadius: 4
+    },
+    rowProduct: {
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 7,
+        marginBottom: 2
+    },
+    productPrice: {
+        color: '#008764',
+        fontFamily: 'poppins_semibold',
+        fontSize: 18,
+        marginBottom: 7
+    },
+    productName: {
+        fontFamily: 'poppins_semibold',
+        color: '#000',
+        fontSize: 12
+    },
+    unit: {
+        color: '#7A7A7A',
+        fontFamily: 'poppins_regular',
+        fontSize: 12
+    },
+    buttonAdd: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 40,
+        backgroundColor: '#E6F5F1',
+        width: '100%',
+        borderRadius: 4
+    },
+    textAdd: {
+        color: '#008764',
+        fontFamily: 'poppins_medium',
+        marginLeft: 5,
+        fontSize: 14
     }
 });
 
