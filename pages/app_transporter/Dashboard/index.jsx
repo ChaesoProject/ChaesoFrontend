381cb647d02678b1a0e5c8fa8b24bf1cb01f0e45
rochamanuela <rochadesouzamanuela@gmail.com>
2024-04-18 15:49:10 -0300

    feat: dashboard created, but the graphic is a image

--- pages/app_transporter/Dashboard/index.jsx
+++ pages/app_transporter/Dashboard/index.jsx
@@ -3,49 +3,42 @@ import {
     Text,
     View,
     ScrollView,
-    TouchableOpacity,
     Image,
-    FlatList,
-    Modal,
 } from "react-native";
 
-// import { Shadow } from 'react-native-shadow-2';
-
-import Feather from "react-native-vector-icons/Feather";
-import Ionicons from "react-native-vector-icons/Ionicons";
-import FontAwesome from "react-native-vector-icons/FontAwesome";
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-
-import InputWhite from "../../../components/InputWhite";
-import InputGray from "../../../components/InputGray";
-import ButtonGreen from "../../../components/ButtonGreen";
 import styles from "./styles";
 
 export default function Dashboard({ navigation }) {
-    const [cpf, setCpf] = useState("");
-
-    const menu = () => {
-        //navigation.navigate("Home");
-        console.log('menu clicado')
-    };
 
     return (
         <View style={styles.container}>
             <View style={styles.containerChild}>
                 <ScrollView style={styles.mainContainer}>
-                    <Text style={styles.categoryTitle}>Dashboard de Entregas</Text>
-                    <Text style={styles.textHeaderAdress}>Gabriel Santana</Text> {/* deverá recebe o nome do entregador */}
-                    <View style={styles.cardsContainer}>
-                        <Text>o conteudo do dashboard vai ficar aqui</Text>
-                    </View>
-                    <View style={styles.resume}>
-                        <View>
-                            <Text style={styles.textFinal}>Valor total</Text>
-                            <Text style={styles.textPriceFinal}>R$ 30,00</Text>
+                    <Text style={styles.title}>Dashboard de Entregas</Text>
+                    <Text style={styles.subtitle}>Gabriel Santana</Text>
+
+                    <View style={styles.containerValues}>
+                        <Image
+                            style={styles.imageGrafic}
+                            source={require('../../../assets/images/graphic_image.png')}
+                        />
+
+                        <View style={styles.containerLastMonths}>
+                            <Text style={styles.smallText1}>Total de entregas nos últimos 4 meses</Text>
+                            <Text style={styles.bigText1}>765</Text>
                         </View>
-                        {/* <TouchableOpacity style={styles.buttonContainer} onPress={() => setModalVisible(true)}>
-                            <Text style={styles.buttonText}>Finalizar</Text>
-                        </TouchableOpacity> */}
+
+                        <View style={styles.containerLastMonthsValue}>
+                            <Text style={styles.smallText2}>Total do valor de entregas nos últimos 4 meses</Text>
+                            <Text style={styles.bigText2}>R$ 15.350,86</Text>
+                        </View>
+
+                        <Text style={styles.titleMedium}>Dashboard Total</Text>
+
+                        <View style={styles.containerTotalOrders}>
+                            <Text style={styles.smallText3}>Total de entregas desde Janeiro de 2023</Text>
+                            <Text style={styles.bigText3}>2850</Text>
+                        </View>
                     </View>
                 </ScrollView>
             </View>
